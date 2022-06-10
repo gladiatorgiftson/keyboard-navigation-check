0b84b87e0e77fef7ae18ac33d70617ddd76a223d
giftson <fgif678@gmail.com>
2022-06-10 19:20:11 +0530

    Changes respective to emualation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import { Button } from "antd";
+import _ from "lodash";
+import "antd/dist/antd.css";
 
 function App() {
+  const [secondaryFilter, setSecondaryFilter] = useState([]);
+  const [activeRow, setActiveRow] = useState();
   const dummyData1 = ["a", "b", "c", "d"];
   // const dummyData1 = false;
   const dummyData2 = ["e", "f", "g", "h"];
@@ -10,27 +15,15 @@ function App() {
   // const dummyData3 = false
   const dummyData4 = ["n", "o", "p", "h"];
   // const dummyData4 = false
+  const filterButton = [
+    { name: "dummyData1" },
+    { name: "dummyData2" },
+    { name: "dummyData3" },
+    { name: "dummyData4" },
+  ];
+  const handleDownPress = () => {};
 
-  const [activeRow, setActiveRow] = useState();
-  const [rowIndex, setRowIndex] = useState([]);
-  const [activeIndex, setActiveIndex] = useState();
-  const [activeRowIndex, setActiveRowIndex] = useState("");
-  const [indexValue, setIndexValue] = useState(0);
-  const [indexLength, setIndexLength] = useState(6);
-  // cosnt [rowName, setName]
-  const handleDownPress = () => {
-    if (rowIndex.isEmpty) {
-      setRowIndex(["vehicle", "customers", "agreements", "reservation"]);
-      setActiveIndex(rowIndex[indexValue]);
-      setIndexValue(0);
-    }
-  };
-
-  const useComponetActive = () => {
-    useEffect(() => {
-      setActiveRowIndex(activeIndex + indexValue);
-    }, [activeIndex, indexValue]);
-  };
+  const handleUpPress = () => {};
 
   const useKeyPress = () => {
     function downHandler({ key }) {
@@ -41,11 +34,14 @@ function App() {
     // If released key is our target key then set to false
     const upHandler = ({ key }) => {
       if (key === "ArrowUp") {
-        console.log("Arrow Up");
+        handleUpPress();
       }
     };
+
     // Add event listeners
     useEffect(() => {
+      console.log(secondaryFilter);
+      console.log("Secondary controller executed");
       window.addEventListener("keydown", downHandler);
       window.addEventListener("keyup", upHandler);
       // Remove event listeners on cleanup
@@ -53,69 +49,104 @@ function App() {
         window.removeEventListener("keydown", downHandler);
         window.removeEventListener("keyup", upHandler);
       };
-    }, [rowIndex]); // Empty array ensures that effect is only run on mount and unmount
+    }, [secondaryFilter]); // Empty array ensures that effect is only run on mount and unmount
+  };
+
+  const removeFromFilter = (name) => {
+    setSecondaryFilter(_.without(secondaryFilter, name));
+  };
+
+  const addToFilter = (name) => {
+    setSecondaryFilter([...secondaryFilter, name]);
   };
 
   useKeyPress();
+
+  useEffect(() => {
+    console.log("Second UseFilter");
+  }, [secondaryFilter]);
   return (
-    <div className="App-container">
-      {dummyData1 &&
-        dummyData1.map((item, index) => {
-          const activeRowIndex = "vehicle" + index;
-          console.log(activeRowIndex);
-          return (
-            <div
-              key={index}
-              className={activeRowIndex === activeRow ? "Active" : "App-Item"}
-            >
-              {item}
-            </div>
-          );
-        })}
-      {dummyData2 &&
-        dummyData2.map((item, index) => {
-          const activeRowIndex = "customers" + index;
-          return (
-            <div
-              key={index}
-              className={activeRowIndex === activeRow ? "Active" : "App-Item"}
+    <>
+      <div className="App-container-button">
+        {filterButton.map((item) => (
+          <div className="App-Button">
+            <Button
+              name={item.name}
               onClick={(e) => {
-                console.log(activeRowIndex);
+                if (secondaryFilter?.includes(item.name)) {
+                  removeFromFilter(item.name);
+                } else {
+                  addToFilter(item.name);
+                }
               }}
+              type={secondaryFilter?.includes(item.name) ? "primary" : "ghost"}
             >
-              {item}
-            </div>
-          );
-        })}
-      {dummyData3 &&
-        dummyData3.map((item, index) => {
-          const activeRowIndex = "Agreements" + index;
-          return (
-            <div
-              key={index}
-              className={activeRowIndex === activeRow ? "Active" : "App-Item"}
-              onClick={(e) => {
-                console.log(activeRowIndex);
-              }}
-            >
-              {item}
-            </div>
-          );
-        })}
-      {dummyData4 &&
-        dummyData4.map((item, index) => {
-          const activeRowIndex = "Reservation" + index;
-          console.log(activeRowIndex);
-          return (
-            <div
-              key={index}
-              className={activeRowIndex === activeRow ? "Active" : "App-Item"}
-            >
-              {item}
-            </div>
-          );
-        })}
-    </div>
+              {item.name}
+            </Button>
+          </div>
+        ))}
+      </div>
+      <div className="App-container">
+        {(secondaryFilter.includes("dummyData1") ||
+          secondaryFilter.length === 0) &&
+          dummyData1.map((item, index) => {
+            const activeRowIndex = "dummyData1" + index;
+            return (
+              <div
+                key={index}
+                className={activeRowIndex === activeRow ? "Active" : "App-Item"}
+              >
+                {item}
+              </div>
+            );
+          })}
+        {(secondaryFilter.includes("dummyData2") ||
+          secondaryFilter.length === 0) &&
+          dummyData2.map((item, index) => {
+            const activeRowIndex = "dummyData2" + index;
+            return (
+              <div
+                key={index}
+                className={activeRowIndex === activeRow ? "Active" : "App-Item"}
+                onClick={(e) => {
+                  console.log(activeRowIndex);
+                }}
+              >
+                {item}
+              </div>
+            );
+          })}
+        {(secondaryFilter.includes("dummyData3") ||
+          secondaryFilter.length === 0) &&
+          dummyData3.map((item, index) => {
+            const activeRowIndex = "dummyData3" + index;
+            return (
+              <div
+                key={index}
+                className={activeRowIndex === activeRow ? "Active" : "App-Item"}
+                onClick={(e) => {
+                  console.log(activeRowIndex);
+                }}
+              >
+                {item}
+              </div>
+            );
+          })}
+        {(secondaryFilter.includes("dummyData4") ||
+          secondaryFilter.length === 0) &&
+          dummyData4.map((item, index) => {
+            const activeRowIndex = "dummyData4" + index;
+            return (
+              <div
+                key={index}
+                className={activeRowIndex === activeRow ? "Active" : "App-Item"}
+              >
+                {item}
+              </div>
+            );
+          })}
+      </div>
+    </>
   );
 }
 
